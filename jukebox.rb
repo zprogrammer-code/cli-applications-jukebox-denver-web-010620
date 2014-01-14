72a7406f72b98cd46fd7a256562db4fe4e9ca227
Arel English <arelenglish@gmail.com>
2014-01-14 14:22:32 -0500

    added jukebox and update readme

---
+++ jukebox.rb
@@ -0,0 +1,10 @@
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapman - Cats in the Cradle"
+]
