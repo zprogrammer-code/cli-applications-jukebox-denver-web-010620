470327e2f077948fe204d44f786c93ccdf74b899
Logan Hasson <logan.hasson@gmail.com>
2014-06-05 19:34:07 -0400

    Remove comments

--- spec/jukebox_spec.rb
+++ spec/jukebox_spec.rb
@@ -36,18 +36,14 @@ describe "CLI Jukebox" do
       expect(output).to match(/Phoenix - 1901/)
     end
  
-    # stub! is deprecated but the test doesnt work with
-    # stub
     it "responds to 'play' and accepts a number as an argument" do
       allow(self).to receive(:gets).and_return("1")
-      # stub!(:gets).and_return("1")
       output = capture_stdout { play(songs) }
       expect(output).to match(/Phoenix - 1901/)
     end
 
     it "responds to 'play' and accepts a full song name as an argument" do
       allow(self).to receive(:gets).and_return("Phoenix - 1901")
-      # stub!(:gets).and_return("Phoenix - 1901")
       output = capture_stdout { play(songs) }
       expect(output).to match(/Phoenix - 1901/)
     end
@@ -60,7 +56,6 @@ describe "CLI Jukebox" do
  
   it "should start the script with the run method" do
     allow(self).to receive(:gets).and_return("exit")
-    # stub!(:gets).and_return("exit")
     output = capture_stdout { run(songs) }
     output.should match(/Please enter a command:/)
   end
