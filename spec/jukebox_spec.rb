f4b299e2a4720a323b95ec3a22d949c37d0ff40f
Logan Hasson <logan.hasson@gmail.com>
2014-06-05 19:33:01 -0400

    Fix specs for RSpec 3.0

--- spec/jukebox_spec.rb
+++ spec/jukebox_spec.rb
@@ -39,14 +39,14 @@ describe "CLI Jukebox" do
     # stub! is deprecated but the test doesnt work with
     # stub
     it "responds to 'play' and accepts a number as an argument" do
-      allow($stdin).to receive(:gets).and_return("1")
+      allow(self).to receive(:gets).and_return("1")
       # stub!(:gets).and_return("1")
       output = capture_stdout { play(songs) }
       expect(output).to match(/Phoenix - 1901/)
     end
 
     it "responds to 'play' and accepts a full song name as an argument" do
-      allow($stdin).to receive(:gets).and_return("Phoenix - 1901")
+      allow(self).to receive(:gets).and_return("Phoenix - 1901")
       # stub!(:gets).and_return("Phoenix - 1901")
       output = capture_stdout { play(songs) }
       expect(output).to match(/Phoenix - 1901/)
@@ -59,7 +59,7 @@ describe "CLI Jukebox" do
   end
  
   it "should start the script with the run method" do
-    allow($stdin).to receive(:gets).and_return("exit")
+    allow(self).to receive(:gets).and_return("exit")
     # stub!(:gets).and_return("exit")
     output = capture_stdout { run(songs) }
     output.should match(/Please enter a command:/)
