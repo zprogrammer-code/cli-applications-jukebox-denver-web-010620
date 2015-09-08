aed8de5b3a83fbb16600301971a30bf7ae5bb122
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-09-08 11:01:38 -0400

    strengthened tests

--- spec/jukebox_spec.rb
+++ spec/jukebox_spec.rb
@@ -1,4 +1,5 @@
 require_relative 'spec_helper'
+require 'pry'
 
 songs = [
   "Phoenix - 1901",
@@ -14,13 +15,11 @@ songs = [
 
 describe "CLI Jukebox" do
 
-  context "with commands" do
-    it "responds to 'help'" do
-      expect { help }.to output(/^(?=.*help)(?=.*list)(?=.*play)(?=.*exit).+/m).to_stdout
-    end
-
-    it "responds to 'list'" do
-      expect { list(songs) }.to output(/Phoenix - 1901/).to_stdout
+  context "methods" do 
+    describe "#help" do 
+      it "lists out the possible commands" do 
+        expect { help }.to output(/^(?=.*help)(?=.*list)(?=.*play)(?=.*exit).+/m).to_stdout
+      end
     end
 
     describe '#play' do
@@ -45,16 +44,38 @@ describe "CLI Jukebox" do
       end
     end
 
-
-    it "responds to 'exit'" do
-      expect { exit_jukebox }.to output(/Goodbye/).to_stdout
+    describe "#list" do
+      it "lists out the available songs" do  
+        expect { list(songs) }.to output(/Phoenix - 1901/).to_stdout
+      end
     end
-  end
 
-  describe '#run' do
+    describe "#exit_jukebox" do 
+      it "terminates the running of the program and outputs 'Goodbye'" do
+        expect { exit_jukebox }.to output(/Goodbye/).to_stdout 
+      end
+    end
+
+    describe '#run' do
     it "starts the program script" do
       allow(self).to receive(:gets).and_return("exit")
       expect { run(songs) }.to output(/Please enter a command:/).to_stdout
+    end
+  end
+
+  end
+
+  context "with commands" do
+    it "responds to 'help'" do
+      self.stub(:gets).and_return("exit")
+      help_output = capture_stdout { run(songs) }
+      expect(help_output).to include("help" && "list" && "play" && "exit")
+    end
+
+    it "responds to 'exit'" do
+      self.stub(:gets).and_return("exit")
+      help_output = capture_stdout { run(songs) }
+      expect(help_output).to include("Goodbye")
     end
   end
 end
