589012d26623afa82fbcff8dfb60263dd49126b8
Zachary Jacobs <skatboyza124@hotmail.com>
2020-01-07 05:28:19 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,22 @@
-# Add your code here
+require 'pry'
+def help 
+puts "I accept the following commands:"
+puts  "- help : displays this help message" 
+puts "- list : displays a list of songs you can play"
+puts "- play : lets you choose a song to play"
+puts "- exit : exits this program"
+end
+def play(songs)
+  puts "Please enter a song name or number:"
+ if  songs = gets.strip
+   puts songs
+end
+  if songs != gets.strip
+   puts "Invalid input, please try again"
+ end
+ if songs = gets.strip
+ puts "Phoenix - 1901"
+end
+end
+#list 
+#exit
